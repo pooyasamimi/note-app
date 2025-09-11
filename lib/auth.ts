3eed485d9db81cfb3d976d02e6c8ad20250b4afa
pooya <poooyasamimi@gmail.com>
2025-09-12 01:36:14 +0330

    up gmail

--- lib/auth.ts
+++ lib/auth.ts
@@ -6,14 +6,17 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
 import { Resend } from "resend";
+import { ur } from "zod/v4/locales";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const auth = betterAuth({
   emailVerification: {
     sendVerificationEmail: async ({ user, url }) => {
+      console.log(url);
+      
       await resend.emails.send({
-        from: "Note App <hello@poooyanoteapp12e32213.dpdns.org>",
+        from: "Note App <support@poooyanoteapp12e32213.dpdns.org>",
         to: [user.email],
         subject: "Verify your email address",
         react: VerificationEmail({ userName: user.name, verificationUrl: url }),
